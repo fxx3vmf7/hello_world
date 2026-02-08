7015710d1c1d688dc23cfd540413220065cd9a39
fxx3vmf7 <youlindi@gmail.com>
2026-02-07 19:11:56 -0500

    Update hello_world.R

--- hello_world.R
+++ hello_world.R
@@ -1,1 +1,22 @@
+<<<<<<< Updated upstream
 print('hello world')
+=======
+print('Hello, Universe')
+
+#volcabulary:
+
+#commit, pull (1st time: clone), push (1st time: publish), remote, repo (local vs remote)
+
+
+# practice vocab:
+
+#Hey Bob! Did you start the analysis yet? If you did, can you publish your repo
+# to Github so that I can clone it to my computer?
+
+# Yes, it's already on Github. But give me until noon to push my latest commits.
+
+#Great! I will pull the repo now to look. At noon, I will clone your more recent commits.
+
+#main, branches
+
+>>>>>>> Stashed changes
